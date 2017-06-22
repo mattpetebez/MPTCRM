4bd656f847f713d2e2285ad92ac4f59f2616bb6e
Matthew Bezuidenhout <mattpetebez@gmail.com>
2017-06-22 11:01:03 +0200

    Added buttons to SalesPersonDetail for adding meetings and sales.

--- SalesForce/SalesForce/settings.py
+++ SalesForce/SalesForce/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'SalesForce',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '2',
     }
 }
 
